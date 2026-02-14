1d12cf67343c41fe2955c5657083e42870a4cc40
portwally <walter.tengler@gmail.com>
2026-02-14 20:45:04 +0000

    Initial Commit

---
+++ ExifChanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExifChanger
+//
+//  Created by Walter Tengler on 14/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExifChanger/ExifChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExifChangerApp.swift
+//  ExifChanger
+//
+//  Created by Walter Tengler on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExifChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
